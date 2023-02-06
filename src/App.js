c2354e1165c46ce86ee3ab640b689b20804be1bc
GAUTAM PAL <gautampal560@gmail.com>
2023-02-06 14:21:19 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import data from './data'
 import List from './List'
 function App() {
   const [people, setPeople] = useState(data)
+  console.log(data[0]);
   return (
     <main>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button onClick={() => setPeople([people[0]])}>clear all</button>
+        <button onClick={() => setPeople([people[1]])}>clear all</button>
       </section>
     </main>
   );
