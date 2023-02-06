923e0fb95d4d5ab49dcd5af8371f61f395991976
GAUTAM PAL <gautampal560@gmail.com>
2023-02-06 13:30:38 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import "./styles.css";
-
-export default function App() {
-  return <div className="App"></div>;
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([people[0]])}>clear all</button>
+      </section>
+    </main>
+  );
 }
+
+export default App;
